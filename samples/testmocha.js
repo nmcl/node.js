9e812a03ced58c4cb97eef025425988cfbfe3e9c
Mark Little <mlittle@redhat.com>
2015-03-12 11:45:00 +0000

    Updated for async

--- samples/testmocha.js
+++ samples/testmocha.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('assert'), fs = require('fs');
 
 describe('Comparing strings', function() {
     describe('when comparing the same strings', function() {
@@ -9,6 +9,17 @@ describe('Comparing strings', function() {
     describe('when comparing different strings', function() {
 	it('should return false', function() {
 	    assert.notStrictEqual("hello", "there");
+	})
+    })
+})
+
+describe('Async testing', function() {
+    describe('When using fs.stat on a file', function() {
+	it('should not be empty', function(done) {
+	    fs.stat('file1.txt', function(err, stat) {
+		assert.notEqual(stat.size, 0);
+		done();
+	    });
 	})
     })
 })
