1e8973c1bd6c584cb94fe50f6238a40608e6f72f
Mark Little <mlittle@redhat.com>
2015-03-12 14:06:24 +0000

    more on web sockets

--- samples/realtime_counter/app.js
+++ samples/realtime_counter/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var count = 0;
 
 var server = http.createServer(function(req, res) {
     fs.readFile('./index.html', function(error, data) {
@@ -13,8 +14,16 @@ console.log('Server running at http://127.0.0.1:3000/');
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
-    console.log('User connected');
+    count++;
+    console.log('User connected. '+count+' user(s) present.');
+
+    socket.emit('users', {number: count});
+    socket.broadcast.emit('users', {number: count});
+
     socket.on('disconnect', function() {
-	console.log('User disconnected');
+	count--;
+	console.log('User disconnected. '+count+' user(s) present.');
+
+	socket.broadcast.emit('users', {number: count});
     });
 });
