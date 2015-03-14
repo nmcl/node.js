59ce0c1200c5d9181091fff1ba4fff74fb66f1fd
Mark Little <mlittle@redhat.com>
2015-03-14 21:14:32 +0000

    push tweets to browser

--- samples/twitter/app.js
+++ samples/twitter/app.js
@@ -1,4 +1,10 @@
-var twitter = require('twitter');
+var express = require('express'),
+  twitter = require('twitter'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
+
+app.set('port', process.env.PORT || 3000);
 
 var twit = new twitter({
   consumer_key: 'LgcXYgdULYjwON5Lyv5SGU8Qk',
@@ -7,8 +13,21 @@ var twit = new twitter({
   access_token_secret: 'MygdEcB2yJcI0suobYgzfLtUi2hmD8MO0eTz5B0XH6bVq'
 });
 
-twit.stream('statuses/filter', { track: 'topgear' }, function(stream) {
+// should use volatile.emit but can't get it to work atm
+twit.stream('statuses/filter', { track: 'nasa' }, function(stream) {
   stream.on('data', function (data) {
-    console.log(data.user.screen_name+' : '+data.text);
+      console.log(data.user.screen_name+' : '+data.text);
+      io.sockets.emit('tweet', {
+	  user: data.user.screen_name,
+	  text: data.text
+      });
   });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
