763dc6d57cbed22909a9836188bd8952b9cfe67f
Mark Little <mlittle@redhat.com>
2015-03-15 15:37:33 +0000

    JSON.stringify

--- samples/json/app.js
+++ samples/json/app.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var obj = {
+    name : "Darth Vader",
+    occupation : "Dark Lord of the Sith",
+    home : "Tatooine"
+}
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type':'application/json'});
-    res.end('{"name":"Darth Vader", "occupation":"Dark Lord of the Sith", "home":"Tatooine"}');
+    res.end(JSON.stringify(obj));
 }).listen(3000, "127.0.0.1");
 
 console.log('Server running at 127.0.0.1:3000/');
