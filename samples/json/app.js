04ec3cf1c893d3b9a53815e1dd97a32b976b804d
Mark Little <mlittle@redhat.com>
2015-03-15 10:02:45 +0000

    json sample

---
+++ samples/json/app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end('{"name":"Darth Vader", "occupation":"Dark Lord of the Sith", "home":"Tatooine"}');
+}).listen(3000, "127.0.0.1");
+
+console.log('Server running at 127.0.0.1:3000/');
