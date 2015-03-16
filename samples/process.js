c797721f6de1c4d81d0734e1f80f7a15d982be91
Mark Little <mlittle@redhat.com>
2015-03-16 11:44:43 +0000

    updated with signal handling.

--- samples/process.js
+++ samples/process.js
@@ -1,1 +1,16 @@
 console.log(process.pid);
+
+process.stdin.resume();
+
+process.on('SIGINT', function() {
+    console.log('Got a SIGINT. Exiting.');
+    process.exit(0);
+});
+
+process.on('SIGTERM', function() {
+    console.log('Got a SIGTERM. Exiting.');
+    process.exit(0);
+});
+
+console.log('Run `kill '+process.pid+'` to send a SIGTERM.');
+console.log('Run `kill -s SIGINT '+process.pid+'` to send a SIGINT.');
