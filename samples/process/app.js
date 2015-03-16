687bd3f4a2b842c5554cbf671198d8552c6cee74
Mark Little <mlittle@redhat.com>
2015-03-16 18:45:49 +0000

    update with signals

--- samples/process/app.js
+++ samples/process/app.js
@@ -5,3 +5,9 @@ ping.stdout.setEncoding('utf8');
 ping.stdout.on('data', function(data) {
     console.log(data);
 });
+
+ping.on('exit', function(code, signal) {
+    console.log('child process was killed with a '+signal+' signal');
+});
+
+ping.kill('SIGINT');
