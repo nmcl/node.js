be19ce98db9eca9bffe287af613cb9710ed82d79
Mark Little <mlittle@redhat.com>
2015-03-19 08:39:53 +0000

    updated to screen_name

--- samples/events/twitter/app.js
+++ samples/events/twitter/app.js
@@ -1,20 +1,20 @@
 describe('OAuth1.0',function(){
   var OAuth = require('oauth');
  
-  it('tests trends Twitter API v1.1',function(done){
+  it('tests screen_nam Twitter API v1.1',function(done){
     var oauth = new OAuth.OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
-      'LgcXYgdULYjwON5Lyv5SGU8Qky',
-      'iIKDlHsIoLgxqYquMWM2ABlWVlU1oyt9iJKRTzs06tmcyMPDyI',
+      'LgcXYgdULYjwON5Lyv5SGU8Qk', // consumer key
+      'iIKDlHsIoLgxqYquMWM2ABlWVlU1oyt9iJKRTzs06tmcyMPDyI', // consumer secret
       '1.0A',
       null,
       'HMAC-SHA1'
     );
     oauth.get(
-      'https://api.twitter.com/1.1/trends/place.json?id=23424977',
-      '16669912-nDwcBlCMEzvWRdB86mOVZlQcvucjN3KQlq4s2fMrg', //test user token 
-      'LH7Bpqhj8fsV4ehLotA3SR5vTm0olwWxmZYAgQ6tO20Al', //test user secret             
+      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=nmcl',
+      '16669912-QFEAwP0sAn3Rix3fSg8djtUSMXttrqq8BypOGsS9p', // access token
+      'ZkukcSZf03n8S4KFX13iJfGW1b6mcFM3dnUEBxWxGq7vq', // access secret
       function (e, data, res){
         if (e) console.error(e);        
         console.log(require('util').inspect(data));
