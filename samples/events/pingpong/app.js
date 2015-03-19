3fd2ef5507b499edaa6e7a393593e3b0070d16cd
Mark Little <mlittle@redhat.com>
2015-03-19 14:45:59 +0000

    added listener dynamically

--- samples/events/pingpong/app.js
+++ samples/events/pingpong/app.js
@@ -19,3 +19,17 @@ pingpong.on('pong', function() {
 	pingpong.emit('ping');
     }, 2000);
 });
+
+var logPing = function() {
+    console.log("Second ping listener got ping");
+};
+
+setTimeout(function() {
+    console.log('Added a second ping listener');
+    pingpong.on('ping', logPing);
+}, 4000);
+
+setTimeout(function() {
+    console.log('Removed second ping listener');
+    pingpong.removeListener('ping', logPing);
+}, 12000);
