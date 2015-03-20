53467bb12908213e8d108e043c70f0f8555e93d9
Mark Little <mlittle@redhat.com>
2015-03-20 21:37:47 +0000

    with encoding

--- samples/buffer/app.js
+++ samples/buffer/app.js
@@ -4,3 +4,8 @@ fs.readFile('file.txt', function(err, data) {
     if (err) throw err;
     console.log(data);
 });
+
+fs.readFile('file.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
