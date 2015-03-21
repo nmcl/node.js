da76c8e1168a923007e94a215de536d81b6313bd
Mark Little <mlittle@redhat.com>
2015-03-21 19:18:53 +0000

    concatenate

--- samples/streams/basic/app.js
+++ samples/streams/basic/app.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
 var stream = fs.ReadStream('names.txt');
+var data = '';
 
 stream.setEncoding('utf8');
 stream.on('data', function(chunk) {
+    data += chunk;
     console.log('read some data');
 });
 stream.on('close', function() {
     console.log('all the data is read');
+    console.log(data);
 });
