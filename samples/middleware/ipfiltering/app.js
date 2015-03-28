8887eb35f99160905ac36311d204de5e6d60c232
Mark Little <mlittle@redhat.com>
2015-03-28 20:39:20 +0000

    ip filtering sample

---
+++ samples/middleware/ipfiltering/app.js
@@ -0,0 +1,28 @@
+var connect = require('connect'), http = require('http');
+var app = connect().use(filterByIp(['::ffff:127.0.0.1'])).use(helloWorld);
+
+function filterByIp(ips) {
+    var ips = ips || [];
+
+    return function(req, res, next) {
+	console.log(ips);
+	console.log(req.connection.remoteAddress);
+	console.log(ips.indexOf(req.connection.remoteAddress));
+	if (ips.indexOf(req.connection.remoteAddress) == -1) {
+	    res.writeHead(401, {'Content-Type':'text/plain'});
+	    res.write('Sorry. You are not allowed to access this server.');
+	    res.end();
+	}
+	else
+	{
+	    next();
+	}
+    };
+};
+
+function helloWorld(req, res) {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('You can view this!');
+};
+
+http.Server(app).listen(3000);
